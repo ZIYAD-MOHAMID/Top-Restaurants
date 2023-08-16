0a960929a47ed167f484d6678af764155421b4ad
Ziyad Mohamid Shokry <125437788+ZIYAD-MOHAMID@users.noreply.github.com>
2023-08-16 18:28:11 +0300

    first commit

---
+++ components/restaurant/Row.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+const props = defineProps<RowProps>();
+interface RowProps {
+  rank?: number;
+  name?: string;
+  index?: number;
+  isHeader?: boolean;
+}
+</script>
+
+<template>
+  <div class="row" v-if="isHeader">
+    <h4 class="header">Rank</h4>
+    <h4 class="header">Chain</h4>
+  </div>
+  <div
+    v-else
+    class="row"
+    :style="
+      index % 2 === 0
+        ? { background: 'rgba(128,128,128,0.15)' }
+        : { background: 'hsla(0, 0%, 62%, 0.819)' }
+    "
+  >
+    <h4 class="header rank">{{ rank }}</h4>
+    <NuxtLink :to="`/restaurants/${name}`" class="header link">{{
+      name
+    }}</NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+.row {
+  border-top: 1px solid rgba(128, 128, 128, 0.336);
+  display: flex;
+  align-items: center;
+}
+.header {
+  font-size: 1.25rem;
+  color: hsla(253, 82%, 30%, 0.819);
+  margin: 0;
+  margin-right: 2rem;
+  padding: 0.5rem 1rem;
+  font-weight: 700;
+  width: 4rem;
+}
+.rank {
+  color: black;
+}
+.link {
+  text-decoration: none;
+  color: rgb(228, 157, 27);
+  width: auto;
+  font-weight: 500;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <Html :lang="dynamic > 50 ? 'en-GB' : 'en-US'">
+      <Head>
+        <Title>Restaurantly </Title>
+        <Meta name="description" content="My page is cool" />
+        <Link
+          rel="preload"
+          href="https://www.iconpacks.net/icons/2/free-sad-face-icon-2691-thumb.png"
+          as="script"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous"
+        />
+      </Head>
+    </Html>
+    <div class="container">
+      <h1>Welcome to Restaurantly</h1>
+      <NuxtLink to="/restaurants">Go to restaurants</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin-top: 5rem;
+}
+</style>
